d16695a7dab973da8a21a0130137f56eadd64893
zhengx-2000 <jp2018212779@qmul.ac.uk>
2021-05-20 19:00:00 +0800

    Infinite Loader Update

--- js/Infinite_drawframe.js
+++ js/Infinite_drawframe.js
@@ -2,12 +2,16 @@
 //var gamectx = game.getContext("2d");
 var countdown = document.getElementById("countdown");
 var countdownctx = countdown.getContext("2d");
+var countdownFactor = 1;
+
+var w = window.innerWidth;
             
 //drawPlayer(gamectx);
 /*drawStairs(gamectx);
 drawNotes(gamectx);*/
 
-drawCountdown(countdownctx);
+adjustCountdownLength();
+drawCountdown(countdownctx, countnub);
 
 /*function drawPlayer(ctx) {
     ctx.fillStyle = "#333";
@@ -33,12 +37,78 @@ function drawNotes(ctx) {
     }
 }
 */
-function drawCountdown(ctx,countnub1) {
+function drawCountdown(ctx, countnub1) {
     ctx.fillStyle = "#333";
     //ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
+    //ctx.textBaseline = "middle";
     //ctx.font = "40px bold Arial";
     //ctx.fillText("Time:", 20, 46);
-    ctx.strokeRect(70, 50, 227, 10);
-    ctx.fillRect(70, 50, countnub1, 10);
+    //ctx.strokeRect(0, 70, 227*countdownFactor, 20);
+    //ctx.fillRect(0, 70, countnub1*countdownFactor, 20);
+    ctx.strokeRect(0, 50, 227*countdownFactor, 20);
+    ctx.fillRect(0, 50, countnub1*countdownFactor, 20);
+}
+
+window.onresize = changeScreen;
+function changeScreen(){
+    adjustCountdownLength();
+}
+
+/*window.onload = function() {
+    var canvas = document.createElement("canvas");
+    canvas.id = "canvas";
+    var box = document.getElementById("countdown");
+    box.appendChild(canvas);
+
+    document.getElementById("canvas").height = 89;
+    var w = window.innerWidth;
+    if(w<600)
+        document.getElementById("canvas").width = 230;
+    if(w>=600&&w<900)
+        document.getElementById("canvas").width = 460;
+    if(w>=900&&w<1200)
+        document.getElementById("canvas").width = 690;
+    if(w>1200)
+        document.getElementById("canvas").width = 920;
+}*/
+
+function adjustCountdownLength() {
+    w = window.innerWidth;
+    console.log("w:" + w);
+    if(w < 600) {
+        countdownFactor = 1;
+        countdown = document.getElementById("countdown");
+        countdown.style.width = 230;
+        countdownctx = countdown.getContext("2d");
+        countdownctx.clearRect(0,0,countdown.width,countdown.height);
+        //countdownctx.strokeRect(0, 50, 400, 20);
+        drawCountdown(countdownctx,countnub);
+    }
+    if(w >= 600 && w < 900) {
+        countdownFactor = 2;
+        countdown = document.getElementById("countdown");
+        countdown.style.width = 460;
+        countdownctx = countdown.getContext("2d");
+        countdownctx.clearRect(0,0,countdown.width,countdown.height);
+        //countdownctx.strokeRect(0, 50, 400, 20);
+        drawCountdown(countdownctx,countnub);
+    }
+    if(w >= 900 && w < 1200) {
+        countdownFactor = 3;
+        countdown = document.getElementById("countdown");
+        countdown.style.width = 690;
+        countdownctx = countdown.getContext("2d");
+        countdownctx.clearRect(0,0,countdown.width,countdown.height);
+        //countdownctx.strokeRect(0, 50, 400, 20);
+        drawCountdown(countdownctx,countnub);
+    }
+    if(w >= 1200) {
+        countdownFactor = 4;
+        countdown = document.getElementById("countdown");
+        countdown.style.width = 920;
+        countdownctx = countdown.getContext("2d");
+        countdownctx.clearRect(0,0,countdown.width,countdown.height);
+        //countdownctx.strokeRect(0, 50, 400, 20);
+        drawCountdown(countdownctx,countnub);
+    }
 }
